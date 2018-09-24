ab5bbee740a0465bd0cbceff15320e5a24a72de1
Pietro Allievi <pietro.allievi@gmail.com>
2018-09-24 21:35:36 +0200

    italian I18N and time zone Rome"

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Ciatt
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    I18n.available_locales = [:en, :it]
+    config.i18n.default_locale = :it
+    config.time_zone = "Rome"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
