90c5ff9296b2f600e263f7b6e419cac44abf8ca7
Pietro Allievi <pietro.allievi@gmail.com>
2020-04-11 08:39:18 +0200

    online and offline messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
 			if @user.authenticate(params[:session][:password])
 				login @user
 				flash[:success] = "Bentornato, #{@user.name}?"
+				user_message "#{@user.name} è di nuovo online"
 				redirect_to root_url
 			else
 				flash[:danger] = "Password per #{params[:session][:name]} non corretta"
@@ -21,7 +22,24 @@ class SessionsController < ApplicationController
 	end
 	
 	def destroy
+		user_message "#{current_user.name} è offline"
 		log_out
 		redirect_to root_url
 	end
+
+	private
+
+	def user_message message_body
+		current_user.chat_users.each do |cu|
+			message = current_user.messages.new
+			message.chat_id = cu.chat_id
+			message.body = message_body
+
+			if message.save
+				ActionCable.server.broadcast 'messages',
+					message: message.as_json
+			end
+		end
+	  end
+  
 end
